f1810e09fb67eaff76b3d640fb1fb13371367483
Koba Leszek <otpusk.kiev@gmail.com>
2017-01-08 19:21:04 +0100

    zmiana button

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by oem on 2017-01-03.
+ */
+
+document.querySelector('.fa-caret-square-o-up').addEventListener("click", function () {
+    $('html,body').animate({scrollTop: 0}, 'slow');
+});
+
+
+
+
