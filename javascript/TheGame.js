6bafde89bd34c9e30927a6b88ab85baa4a746a99
Piotr Łyczko <lyczkop@gmail.com>
2017-01-13 21:28:21 +0100

    Beta PopulationRange with map

--- javascript/TheGame.js
+++ javascript/TheGame.js
@@ -1,3 +1,4 @@
+/*------------------------The Game------------------------*/
 window.onload = function () {
     var displayCountries = function (allCountries) {
         // run game and create forms
@@ -6,27 +7,88 @@ window.onload = function () {
 
         var countrie = allCountries.map(function (countrie) {
             return {
+                code: countrie.numericCode,
                 name: countrie.name,
                 capitol: countrie.capital,
                 population: countrie.population
             }
         })
 
-        var random = Math.floor((Math.random() * 250) + 1);
-        console.log(countrie[random].population);
+        $("input").prop('disabled', false); // disabel input.range before load data
 
-        document.querySelector('#myRange').addEventListener('change', showDivs);
-        function showDivs(event) {
-            console.log(event)
+        var random = Math.floor((Math.random() * 250) + 1); // random number from 1 to 250 (250 = all cuntires)
+        var countrieQuest = countrie[random]; // random countrie to the game 178 = Poland index
+        var population = countrieQuest.population; // select population of the countrieQuest
+
+        console.log(population);
+        var poland = countrie.filter(function( obj ) {
+            return obj.name == 'Poland';
+        });
+        console.log('code: ' + countrieQuest.code + '\nKraj: ' + countrieQuest.name + ' \nStolica:' + countrieQuest.capitol);
+
+        $('#countryQuest').html(' ' + countrieQuest.name); // insert name of countrie
+        minVal = Math.round(population/1000)*1000/10; // radnom MIN value of population to choose
+        maxVal = Math.round(population/1000)*1000*3; //radnom MAX value of population to choose
+        $('#myRange').attr('min', minVal); // add MIN value attribute to input.range
+        $('#minPop').html(numeral(minVal).format('0,000.00a')); // display MIN value before input.range
+        $('#myRange').attr('max', maxVal); // add MAX value attribute to input.range
+        $('#maxPop').html(numeral(maxVal).format('0,000.00a')); // display MAX value before input.range
+
+        console.log($('#myRange').attr('min'));
+        console.log($('#myRange').attr('max'));
+
+
+        minRightAnswer = population - (population/100*20); // select MIN value to correct answer
+        maxRightAnswer = population + (population/100*20); // select MAX value to right answer
+        console.log(minRightAnswer + ' - ' + population + ' - ' + maxRightAnswer); // display range of correct value
+
+
+        /**displays current values selected**/
+        $("#myRange").on('mousedown', function() {
+            $("#myRange").on('mousemove', function () {
+                $('#answer').html(numeral($(this).val()).format('0,000.00a'));
+            })
+        });
+
+
+        /**************************
+         ** CHECK CORRECT ANSWER **
+         **************************/
+        $('#ansBtn').click(function () {
+            var answer = $('#myRange').val();
+            if (answer > minRightAnswer && answer < maxRightAnswer){
+                console.log('DOBRZE!');
+                alert('WYGRAŁEŚ!')
+
+            }else {
+                console.log('ŹLE!');
+                alert('BUuu cienias!')
+            }
+        });
+        /** CHECK CORRECT ANSWER **/
+
+
+        /************************
+         ***** GOOGLE  MAP ******
+         ************************/
+        google.charts.load('upcoming', {'packages':['geochart']});
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+        function drawRegionsMap() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Country', 'Population'],
+                [countrieQuest.name, countrieQuest.population]
+            ]);
+
+            var options = {};
+
+            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+            chart.draw(data, options);
         }
-
-        // countrie.forEach(function (element) {
-        //     if (element.population == 0)
-        //     console.log(element);
-        // })
-        // console.log(Math.floor((Math.random() * 250) + 1));
-
-    };
+        /****** GOOGLE  MAP END ******/
+    }; //end of displayCountries
 
     getAllCountriesFromApi(displayCountries);
 };
