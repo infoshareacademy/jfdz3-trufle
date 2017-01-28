6e15c6b3393bbc3924ab8a75a85586e2350ef9af
Piotr Łyczko <lyczkop@gmail.com>
2017-01-28 14:22:30 +0100

    Basic game without visual efects done.

--- javascript/population.js
+++ javascript/population.js
@@ -1,3 +1,65 @@
+var random = Math.floor((Math.random() * 250) + 1); // random number from 1 to 250 (250 = all cuntires)
+// random = 178; // index Polski
+/** INIT Google Map 2 **/
+
+function initMap() {
+
+    var displayCountries2 = function (allCountries) {
+
+        var countrie = allCountries.map(function (countrie) {
+            return {
+                code: countrie.numericCode,
+                name: countrie.name,
+                capitol: countrie.capital,
+                population: countrie.population,
+                latlng: countrie.latlng,
+                shortName: countrie.alpha2Code,
+                area: countrie.area
+            }
+        })
+
+        var lat = countrie[random].latlng[0];
+        var lng = countrie[random].latlng[1];
+        var shortName = countrie[random].shortName;
+        var area = countrie[random].area;
+        var zoom;
+        if (area < 5000) {
+            zoom = 8;
+        } else if (area < 20000) {
+            zoom = 6;
+        } else if (area < 100000) {
+            zoom = 5;
+        } else if (area < 700000) {
+            zoom = 4;
+        } else if (area < 1050000) {
+            zoom = 3;
+        }
+
+        console.log(lat + ' ' + lng);
+        console.log(area);
+        console.log(zoom);
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: new google.maps.LatLng(lat, lng),
+            zoom: zoom,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var layer = new google.maps.FusionTablesLayer({
+            query: {
+                select: 'geometry',
+                from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk',
+                where: "ISO_2DIGIT IN ('" + shortName + "')"
+            },
+            map: map,
+            suppressInfoWindows: true
+        });
+        layer.setMap(map);
+    }
+    /** INIT Google Map 2 END **/
+    getAllCountriesFromApi(displayCountries2);
+}
+
 /*------------------------The Game------------------------*/
 window.onload = function () {
     var displayCountries = function (allCountries) {
@@ -16,15 +78,15 @@ window.onload = function () {
 
         $("input").prop('disabled', false); // disabel input.range before load data
 
-        var random = Math.floor((Math.random() * 250) + 1); // random number from 1 to 250 (250 = all cuntires)
+        // var random = Math.floor((Math.random() * 250) + 1); // random number from 1 to 250 (250 = all cuntires)
         var countrieQuest = countrie[random]; // random countrie to the game 178 = Poland index
         var population = countrieQuest.population; // select population of the countrieQuest
 
         console.log('code: ' + countrieQuest.code + '\nKraj: ' + countrieQuest.name + ' \nStolica:' + countrieQuest.capitol);
 
         $('#countryQuest').html(' ' + countrieQuest.name); // insert name of countrie
-        minVal = Math.round(population/1000)*1000/10; // radnom MIN value of population to choose
-        maxVal = Math.round(population/1000)*1000*3; //radnom MAX value of population to choose
+        minVal = Math.round(population / 1000) * 1000 / 10; // radnom MIN value of population to choose
+        maxVal = Math.round(population / 1000) * 1000 * 3; //radnom MAX value of population to choose
         $('#myRange').attr('min', minVal); // add MIN value attribute to input.range
         $('#minPop').html(numeral(minVal).format('0,000.00a')); // display MIN value before input.range
         $('#myRange').attr('max', maxVal); // add MAX value attribute to input.range
@@ -34,13 +96,13 @@ window.onload = function () {
         console.log($('#myRange').attr('max'));
 
 
-        minRightAnswer = population - (population/100*20); // select MIN value to correct answer
-        maxRightAnswer = population + (population/100*20); // select MAX value to right answer
+        minRightAnswer = population - (population / 100 * 20); // select MIN value to correct answer
+        maxRightAnswer = population + (population / 100 * 20); // select MAX value to right answer
         console.log(minRightAnswer + ' - ' + population + ' - ' + maxRightAnswer); // display range of correct value
 
 
         /**displays current values selected**/
-        $("#myRange").on('mousedown', function() {
+        $("#myRange").on('mousedown', function () {
             $("#myRange").on('mousemove', function () {
                 $('#answer').html(numeral($(this).val()).format('0,000.00a'));
             })
@@ -52,11 +114,11 @@ window.onload = function () {
          **************************/
         $('#ansBtn').click(function () {
             var answer = $('#myRange').val();
-            if (answer > minRightAnswer && answer < maxRightAnswer){
+            if (answer > minRightAnswer && answer < maxRightAnswer) {
                 console.log('DOBRZE!');
                 alert('WYGRAŁEŚ!')
 
-            }else {
+            } else {
                 console.log('ŹLE!');
                 alert('BUuu cienias!')
             }
@@ -64,66 +126,37 @@ window.onload = function () {
         /** CHECK CORRECT ANSWER **/
 
 
-        /************************
+        /*        /!************************
          ***** GOOGLE  MAP ******
-         ************************/
-        google.charts.load('upcoming', {'packages':['geochart']});
-        google.charts.setOnLoadCallback(drawRegionsMap);
+         ************************!/
+         google.charts.load('upcoming', {'packages': ['geochart']});
+         google.charts.setOnLoadCallback(drawRegionsMap);
 
-        function drawRegionsMap() {
+         function drawRegionsMap() {
 
-            var data = google.visualization.arrayToDataTable([
-                ['Country', 'Population'],
-                [countrieQuest.name, countrieQuest.population]
-            ]);
+         var data = google.visualization.arrayToDataTable([
+         ['Country', 'Population'],
+         [countrieQuest.name, countrieQuest.population]
+         ]);
 
-            var options = {tooltip: {trigger: 'none'},
-                            legend: {textStyle: {fontSize: .1} }};
+         var options = {
+         tooltip: {trigger: 'none'},
+         legend: {textStyle: {fontSize: .1}}
+         };
 
-            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+         var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
-            chart.draw(data, options);
-        }
-        /****** GOOGLE MAP END ******/
+         chart.draw(data, options);
+         }
+
+         /!****** GOOGLE MAP END ******!/*/
 
         /** Google Map 2 **/
         $('body').append('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBKljXf4TroNrn0vwSpdLiC5bZF1htiO9Y&callback=initMap" async defer></script>');
         /** Google Map 2 END **/
 
-    }; //end of displayCountries
 
+    }; //end of displayCountries
+    // initMap(50,20);
     getAllCountriesFromApi(displayCountries);
 };
-
-function getAllCountriesFromApi(callback) {
-    var allCountries = [];
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            allCountries = JSON.parse(this.responseText);
-            callback(allCountries);
-        }
-    };
-    xhttp.open("GET", "https://restcountries.eu/rest/v1/all", true);
-    xhttp.send() ;
-
-}
-/** INIT Google Map 2 **/
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: new google.maps.LatLng(30,0),
-        zoom: 2,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    var layer = new google.maps.FusionTablesLayer({
-        query: {
-            select: 'geometry',
-            from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk',
-            where: "ISO_2DIGIT IN ('US', 'GB', 'DE', 'PL')"
-        },
-        map: map,
-        suppressInfoWindows: true
-    });
-    layer.setMap(map);
-}/** INIT Google Map 2 END **/
